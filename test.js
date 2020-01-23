a6e36db24422dfcf25a4602a5bd33277d756950d
Davide D'Antonio <davide.dantonio@webeetle.com>
2020-01-23 16:20:14 +0100

    test

---
+++ test.js
@@ -0,0 +1,103 @@
+'use strict'
+
+const t = require('tap')
+const { test } = t
+const Fastify = require('fastify')
+
+const axiosPlugin = require('./index')
+
+test('fastify.axios intance and instance.methods exists', t => {
+  t.plan(11)
+  const fastify = Fastify()
+  fastify.register(axiosPlugin, {})
+
+  fastify.ready(err => {
+    t.error(err)
+    t.ok(fastify.axios)
+    t.ok(fastify.axios.post)
+    t.ok(fastify.axios.get)
+    t.ok(fastify.axios.patch)
+    t.ok(fastify.axios.options)
+    t.ok(fastify.axios.put)
+    t.ok(fastify.axios.getUri)
+    t.ok(fastify.axios.delete)
+    t.ok(fastify.axios.head)
+    t.ok(fastify.axios.request)
+
+    fastify.close()
+  })
+})
+
+test('fastify.axios should be an instance Axios', t => {
+  t.plan(2)
+  const fastify = Fastify()
+  fastify.register(axiosPlugin)
+
+  fastify.ready(err => {
+    t.error(err)
+    t.ok(fastify.axios instanceof Function)
+
+    fastify.close()
+  })
+})
+
+test('fastify.axios works well', t => {
+  t.plan(3)
+  const fastify = Fastify()
+  fastify.register(axiosPlugin)
+
+  fastify.ready(async err => {
+    t.error(err)
+
+    const { data, status } = await fastify.axios.get('https://nodejs.org/en/')
+    t.ok(data)
+    t.equal(status, 200)
+
+    fastify.close()
+  })
+})
+
+test('fastify.axios register multiple clients', t => {
+  t.plan(24)
+  const fastify = Fastify()
+  const clients = {
+    nodejs: {
+      baseUrl: 'https://nodejs.org'
+    },
+    google: {
+      baseUrl: 'https://google.com'
+    }
+  }
+
+  fastify.register(axiosPlugin, { clients: clients })
+
+  fastify.ready(async err => {
+    t.error(err)
+    t.ok(fastify.axios.nodejs)
+    t.ok(fastify.axios.nodejs instanceof Function)
+    t.ok(fastify.axios.nodejs.post)
+    t.ok(fastify.axios.nodejs.get)
+    t.ok(fastify.axios.nodejs.patch)
+    t.ok(fastify.axios.nodejs.options)
+    t.ok(fastify.axios.nodejs.put)
+    t.ok(fastify.axios.nodejs.getUri)
+    t.ok(fastify.axios.nodejs.delete)
+    t.ok(fastify.axios.nodejs.head)
+    t.ok(fastify.axios.nodejs.request)
+
+    t.ok(fastify.axios.google)
+    t.ok(fastify.axios instanceof Object)
+    t.ok(fastify.axios.google instanceof Function)
+    t.ok(fastify.axios.google.post)
+    t.ok(fastify.axios.google.get)
+    t.ok(fastify.axios.google.patch)
+    t.ok(fastify.axios.google.options)
+    t.ok(fastify.axios.google.put)
+    t.ok(fastify.axios.google.getUri)
+    t.ok(fastify.axios.google.delete)
+    t.ok(fastify.axios.google.head)
+    t.ok(fastify.axios.google.request)
+
+    fastify.close()
+  })
+})
